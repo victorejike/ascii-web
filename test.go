172c8b47731555fba553b70b82bc237d166acdae
victor ejike <victorejiki@gmail.com>
2026-06-08 00:28:48 +0100

    test option

---
+++ test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	time := 20
+
+	if time > 18 {
+		fmt.Println("go eveing how are you doing")
+	} else {
+		fmt.Println("go morning ladies and gental men")
+	}
+}
