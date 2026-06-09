c508aae7ab2899027af9418c7e88a1e2fa354246
victor ejike <victorejiki@gmail.com>
2026-06-09 09:58:33 +0100

    ascii-art

--- test.go
+++ test.go
@@ -1,15 +1,1 @@
-package main
 
-import (
-	"fmt"
-)
-
-func main() {
-	result := [2]string{"victor", "ejike"}
-
-	fmt.Println(result, result[0], result[len(result)-1])
-
-	result[1] = "nmesomma"
-
-	fmt.Println(result)
-}
